4f0678380af246c7e7aa3dfb30417ed30cdc48f0
SOVEREIGN <isimemeni@gmail.com>
2019-11-09 17:01:14 -0500

    added database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,28 @@
-var mySql = require("mysql");
+var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+
+var connection = mysql.createConnection({
+    port     : 3306,
+    host     : 'localhost',
+    user     : 'root',
+    password : 'secret',
+    database : "bamazonDB"
+  });
+   
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
+
+  inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  });
---
+++ bamazonDB.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
